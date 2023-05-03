fd8ad62439a317d27cdaaeb2029e28e099e4870f
jaco718v <jaco718v@stud.kea.dk>
2023-05-03 20:34:50 +0200

    Added security-stuff to events endpoints

--- src/main/java/dat3/amager_records_backend/api/EventController.java
+++ src/main/java/dat3/amager_records_backend/api/EventController.java
@@ -3,8 +3,10 @@ package dat3.amager_records_backend.api;
 import dat3.amager_records_backend.dto.EventRequest;
 import dat3.amager_records_backend.dto.EventResponse;
 import dat3.amager_records_backend.service.EventService;
+import jakarta.annotation.security.PermitAll;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -26,26 +28,28 @@ public class EventController {
     return eventService.getEventsTotal();
   }
 
+
   @GetMapping("{id}")
   public EventResponse getEventById(Pageable pageable, @PathVariable long id){
     return eventService.getEventById(id);
   }
 
+
   @GetMapping
   public List<EventResponse> getAllEvents(Pageable pageable){
     return eventService.getAllEvents(pageable);
   }
-
+  @PreAuthorize("hasAuthority('ADMIN')")
   @PostMapping
   public EventResponse createEvent(@RequestBody EventRequest body){
     return eventService.createEvent(body);
   }
-
+  @PreAuthorize("hasAuthority('ADMIN')")
   @PutMapping("{id}")
   public EventResponse updateEvent(@RequestBody EventRequest body, @PathVariable long id){
     return eventService.updateEvent(body,id);
   }
-
+  @PreAuthorize("hasAuthority('ADMIN')")
   @DeleteMapping("{id}")
   public ResponseEntity<String> deleteEvent(@PathVariable long id){
     return eventService.deleteEvent(id);
